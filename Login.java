e1bbdb2ffc16662795b9aa0cfe9fe419784de5a5
Aastha <78609669+Aastha2345@users.noreply.github.com>
2022-12-07 23:11:33 +0530

    Update Login.java

--- Login.java
+++ Login.java
@@ -11,28 +11,29 @@ public class Login implements ActionListener {
     JButton resetButton = new JButton("Reset");
     JTextField userIDField = new JTextField();
     JPasswordField userPasswordField = new JPasswordField();
-    JLabel userIDLabel = new JLabel("userId:");
+    JLabel userIDLabel = new JLabel("UserId:");
     JLabel userPasswordLabel = new JLabel("Password:");
     JLabel messageLabel = new JLabel();
+
     HashMap<String,String> logininfo = new HashMap<String,String>();
 
     Login(HashMap<String,String> loginInfoOriginal) {
         logininfo = loginInfoOriginal;
 
-        userIDLabel.setBounds(50,100,75,25);
-        userPasswordLabel.setBounds(50,150,75,25);
+        userIDLabel.setBounds(750,400,75,25);
+        userPasswordLabel.setBounds(750,450,75,25);
 
-        messageLabel.setBounds(125,250,500,35);
+        messageLabel.setBounds(825,600,500,35);
         messageLabel.setFont(new Font(null,Font.ITALIC,25));
 
-        userIDField.setBounds(125,100,200,25);
-        userPasswordField.setBounds(125,150,200,25);
+        userIDField.setBounds(875,400,200,25);
+        userPasswordField.setBounds(875,450,200,25);
 
-        loginButton.setBounds(125,200,100,25);
+        loginButton.setBounds(825,500,100,25);
         loginButton.setFocusable(false);
         loginButton.addActionListener(this);
 
-        resetButton.setBounds(325,200,100,25);
+        resetButton.setBounds(950,500,100,25);
         resetButton.setFocusable(false);
         resetButton.addActionListener(this);
 
@@ -45,7 +46,7 @@ public class Login implements ActionListener {
         frame.add(resetButton);
 
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(420,420);
+        frame.setSize(1920,1080);
         frame.setLayout(null);
         frame.setVisible(true);
     }
